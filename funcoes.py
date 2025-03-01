e35fd3c55ec8cd5deab63555f4e280300391ccb9
Samanta Sobral <samantasobral.ds@gmail.com>
2025-03-01 09:15:54 -0300

    Update funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -33,36 +33,20 @@ def filtra_df(df):
 
        return customers_df_filtred, sellers_df_filtred, estados_selecionados
 
-def filtro_checkbox_df(df):
-    # Side Bar:
-    st.sidebar.header('Selecione os Estados')
-
-    # Filtro:
-    lista_estados = sorted(list(df['seller_state'].unique()))
-    estados_selecionados = []
-
-    # Criação das checkboxes para cada estado
-    for estado in lista_estados:
-        if st.sidebar.checkbox(estado, value=True):
-            estados_selecionados.append(estado)
-
-    customers_df_filtred = df[df['customer_state'].isin(estados_selecionados)]
-    sellers_df_filtred = df[df['seller_state'].isin(estados_selecionados)]
-
-    return customers_df_filtred, sellers_df_filtred, estados_selecionados
-
 def bignumbers(c_df, s_df):
        st.subheader('Indicadores Gerais')   #Subtítulo da Sessão de Big Numbers
 
        total_vendas = c_df['total_price'].sum()
        total_customers = c_df['customer_unique_id'].nunique()
        total_sellers = s_df['seller_id'].nunique()
+       total_categorias = c_df['product_category_name'].nunique()
 
-       col1, col2, col3 = st.columns(3)    #Cria 3 colunas
+       col1, col2, col3, col4 = st.columns(4)    #Cria 4 colunas
 
        col1.metric('Total de Vendas', f'R$ {total_vendas:,.2f}')
        col2.metric('Quantidade de Clientes Únicos', f'{total_customers:,.0f}')
        col3.metric('Quantidade de Vendedores Únicos', f'{total_sellers:,.0f}')
+       col4.metric('Quantidade de Categorias', f'{total_categorias:,.0f}') 
 
        return None
 
